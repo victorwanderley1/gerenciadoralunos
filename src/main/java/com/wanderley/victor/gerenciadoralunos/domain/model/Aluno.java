e540ad0ad424803c69603aa15495fbe902095f64
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-03-17 11:43:03 -0300

    Construindo Classe Aluno

--- src/main/java/com/wanderley/victor/gerenciadoralunos/domain/model/Aluno.java
+++ src/main/java/com/wanderley/victor/gerenciadoralunos/domain/model/Aluno.java
@@ -4,5 +4,38 @@ package com.wanderley.victor.gerenciadoralunos.domain.model;
  * @author Victor Wanderley
  */
 public class Aluno {
+    private Long id;
+    private String nome;
+    private Integer idade;
+    
+    public Aluno(String nome, Integer idade, Long id){
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
     
 }
