b6763183301b0a316e68fb237e2b4aa29fa923da
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-03-17 05:16:10 -0300

    Iniciando repositório add arquivos base

---
+++ src/main/java/com/wanderley/victor/gerenciadoralunos/GerenciadoralunosApplication.java
@@ -0,0 +1,13 @@
+package com.wanderley.victor.gerenciadoralunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadoralunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadoralunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanderley/victor/gerenciadoralunos/GerenciadoralunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderley.victor.gerenciadoralunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadoralunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
